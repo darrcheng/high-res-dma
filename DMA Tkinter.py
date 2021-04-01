46dfee742952d06cb3cfd0df3c1f764cb28b00a4
darrcheng <78453696+darrcheng@users.noreply.github.com>
2021-04-01 11:43:06 -0400

    Commented code
    Commented DMA Tkinter code, deleted some lines. Try running it, and also re-organize lines

--- DMA Tkinter.py
+++ DMA Tkinter.py
@@ -1,27 +1,26 @@
-#import u3 #Imports Labjack U3 Class
+#Import Labjack T7 Class
 from labjack import ljm
-import struct
+
+#Imports date/time modules (for pulling system time)
 from datetime import datetime #Pulls current time from system
 from datetime import timedelta #Calculates difference in time
 
+#Imports Tkinter Modules (for GUIs)
 from tkinter import *
 from tkinter import ttk
 root=Tk()
 
-from random import random #TEMP
-
-import threading
-import time
-
+#Imports CSV module for writing to file
 import csv
 
+#Imports MatPlotLib for plotting, extra imports to function with Tkinter
 import matplotlib
 matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 
-
+#Set Input Variables 
 #Declare Streaming Interval, Set Default Value
 streamingInterval = StringVar()
 streamingInterval.set(1000) #Default Value 1000ms 
@@ -32,24 +31,18 @@ voltage_start.set(0) #Default Value 0V
 voltage_stop = StringVar()
 voltage_stop.set(400) #Default Value 500V
 
-#TKINTER defining the callback function (observer) 
+#TKINTER defining the callback function (observer), allows Tkinter to auto pull in changes to fields
 def my_callback(var,index,mode): 
     print("Streaming intverval: {}".format(streamingInterval.get()))
     print("Start Voltage: {}".format(voltage_start.get()))
     print("Stop Voltage: {}".format(voltage_stop.get()))
 
-def set_voltage(voltage, handle, name):
-    signal_factor = 1/2000
-    voltage_output = voltage * signal_factor
-    #Labjack code here to set voltage
-    
+#Function to read the voltage from the Labjack, includes a scaling factor for conversions    
 def read_voltage(instrument, handle, name = 'AIN0', scaling = 1):
      result = ljm.eReadName(handle, name)
      instrument = instrument.append(result * scaling) 
 
-def start_run():
-    return True
-
+#Function to add timestamp to each reading and calculate the time from start of the run
 def time_tracker(exact_time, time_list):
     current_time = datetime.now()
     exact_time.append(current_time)
@@ -74,12 +67,6 @@ canvas = FigureCanvasTkAgg(fig, master=GraphFrame)
 canvas.draw()
 canvas.get_tk_widget().pack()
 figure1 = fig.add_subplot(111)
-
-
-# #Create frame for graph buttons
-# ButtonFrame = ttk.Frame(root)
-# ButtonFrame.grid(row=4, column=0,rowspan=2,padx=0)
-
 
 #Frame for setting logging frequency
 FreqFrame = ttk.Frame(root)
@@ -130,6 +117,8 @@ electrometer_output.grid(row=5, column=5) #change t1 to ElectroVoltage
 electrometer_output.insert('1.0', '0.00')
 ttk.Label(BertanFrame, text="Volts").grid(row=5, column=6, padx=0)
 
+################################# Labjack ########################################
+
 #Open Labjack
 handle = ljm.openS("T7", "ANY", "ANY")  # T7 device, Any connection, Any identifier
 info = ljm.getHandleInfo(handle)
@@ -137,7 +126,7 @@ print("Opened a LabJack with Device type: %i, Connection type: %i,\n"
     "Serial number: %i, IP address: %s, Port: %i,\nMax bytes per MB: %i" %
     (info[0], info[1], info[2], ljm.numberToIP(info[3]), info[4], info[5]))
 
-
+#Function that acts as the main body of the code
 def run_program():
 
     #Initalize Lists
@@ -146,18 +135,12 @@ def run_program():
     dma_voltage = []
     electrometer_voltage = []    
 
-
-
     #Define Labjack Inputs
     electrometer_read = 'AIN0'
     dma_read = 'AIN1'
     dma_write = 'TDAC0'
-    # fig = Figure(figsize=(5, 4), dpi=100)
-    # #fig.add_subplot(111).plot(dma_voltage, electrometer_voltage)
 
-    # canvas = FigureCanvasTkAgg(fig, master=GraphFrame)
-
-    #Pull operating parameters from GUI
+    #Read into the function the operating parameters from GUI
     current_voltage = int(voltage_start.get())
     voltage_end = int(voltage_stop.get())
     step_time = int(streamingInterval.get())
@@ -169,19 +152,15 @@ def run_program():
     gui_filename.delete('1.0','1.end')
     gui_filename.insert('1.0', filename)
 
-    event=threading.Event()
 
-    #Clear Figure
-#    figure1.cla()
-
-    #open file
+    #Open CSV file
     with open(filename, 'w', newline='') as csvfile:
         data_writer = csv.writer(csvfile, delimiter=',')
         
-        #write header
+        #Write CSV file header header
         data_writer.writerow(['Time', 'Time Since Start', 'DMA Voltage', 'Electrometer Voltage'])
         
-
+        #Loop through voltages
         while (current_voltage <= voltage_end):
 
             #Take Readings
@@ -199,24 +178,20 @@ def run_program():
             data_writer.writerow([exact_time[-1], time_from_start[-1], dma_voltage[-1], electrometer_voltage[-1]])
 
             root.after(int(step_time*50/52.458))
-            # time.sleep(1)
             root.update()
 
             #Set voltage
             ljm.eWriteName(handle, dma_write, current_voltage/200)
 
             #Update graphs
-            #fig = Figure(figsize=(5, 4), dpi=100)
             figure1.plot(dma_voltage, electrometer_voltage)
-
-            #canvas = FigureCanvasTkAgg(fig, master=GraphFrame)  # A tk.DrawingArea.
             canvas.draw()
             canvas.get_tk_widget().pack()
 
             #Increment Current Voltage
             current_voltage += 1
         
-        #Reset Voltage to 0
+        #Reset Voltage to 0 at the end of a run
         ljm.eWriteName(handle, dma_write, 0)
 
 
