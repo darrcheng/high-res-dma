9c4ee3c488f9bc78e4c0737d6e8f52bdcc084f3c
darrcheng <dhcheng@andrew.cmu.edu>
2023-02-16 14:46:45 -0500

    added post graphing

--- DMA Control.py
+++ DMA Control.py
@@ -18,6 +18,12 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
 import configparser
 
+import pandas as pd
+import altair as alt
+import webbrowser
+import os
+
+
 root = Tk()
 
 config_file = configparser.ConfigParser()
@@ -121,6 +127,9 @@ def stop_run():
     global interrupt
     interrupt = True
     BertanVoltSet.configure(text="Start", command=start_run)
+    df = pd.read_csv(run_filename_avg)
+    graph_dma_voltage(df)
+
     # Reset Voltage to 0 at the end of a run
 
 
@@ -682,6 +691,101 @@ def ultravolt_voltage_set(voltage_set, lj_handle, neg_output, pos_output):
     if voltage_set == 0:
         ljm.eWriteName(lj_handle, pos_output, 0)
         ljm.eWriteName(lj_handle, neg_output, 0)
+
+
+
+def graph_dma_voltage(df):
+    # Create a selection that chooses the nearest point & selects based on x-value
+    nearest = alt.selection(type='single', nearest=True, on='mouseover',
+                            fields=['DMA Voltage'], empty='none')
+
+    line = alt.Chart(df).mark_line().encode(
+        x='DMA Voltage:Q',
+        y='Electrometer Concentration:Q',
+    )
+
+    # Transparent selectors across the chart. This is what tells us
+    # the x-value of the cursor
+    selectors = alt.Chart(df).mark_point().encode(
+        x='DMA Voltage:Q',
+        tooltip=['DMA Voltage:Q','Electrometer Concentration'],
+        opacity=alt.value(0),
+    ).add_selection(
+        nearest
+    )
+
+    # Draw points on the line, and highlight based on selection
+    points = line.mark_point().encode(
+        opacity=alt.condition(nearest, alt.value(1), alt.value(0))
+    )
+
+    # # Draw text labels near the points, and highlight based on selection
+    # text = line.mark_text(align='left', dx=5, dy=-5).encode(
+    #     text=alt.condition(nearest, 'DMA Voltage:Q', alt.value(' '))
+    # )
+
+    # Draw a rule at the location of the selection
+    rules = alt.Chart(df).mark_rule(color='gray').encode(
+        x='DMA Voltage:Q',
+    ).transform_filter(
+        nearest
+    )
+
+    # Put the five layers into a chart and bind the data
+    dma_voltage = alt.layer(
+        line, selectors, points, rules, #text
+    ).properties(
+        width=600, height=300
+    )
+    ##########################
+   
+        # Create a selection that chooses the nearest point & selects based on x-value
+    nearest = alt.selection(type='single', nearest=True, on='mouseover',
+                            fields=['Time Since Start'], empty='none')
+
+    line = alt.Chart(df).mark_line().encode(
+        x='Time Since Start:Q',
+        y='Electrometer Concentration:Q',
+    )
+
+    # Transparent selectors across the chart. This is what tells us
+    # the x-value of the cursor
+    selectors = alt.Chart(df).mark_point().encode(
+        x='Time Since Start:Q',
+        tooltip=['Time Since Start:Q','Electrometer Concentration'],
+        opacity=alt.value(0),
+    ).add_selection(
+        nearest
+    )
+
+    # Draw points on the line, and highlight based on selection
+    points = line.mark_point().encode(
+        opacity=alt.condition(nearest, alt.value(1), alt.value(0))
+    )
+
+    # # Draw text labels near the points, and highlight based on selection
+    # text = line.mark_text(align='left', dx=5, dy=-5).encode(
+    #     text=alt.condition(nearest, 'DMA Voltage:Q', alt.value(' '))
+    # )
+
+    # Draw a rule at the location of the selection
+    rules = alt.Chart(df).mark_rule(color='gray').encode(
+        x='Time Since Start:Q',
+    ).transform_filter(
+        nearest
+    )
+
+    # Put the five layers into a chart and bind the data
+    time_basis = alt.layer(
+        line, selectors, points, rules, #text
+    ).properties(
+        width=600, height=300
+    )
+    graphs = alt.vconcat(dma_voltage,time_basis)
+   
+    graphs.save(run_filename_avg[:-4]+ '.html')
+    webbrowser.open_new_tab('file://' + os.path.realpath(run_filename_avg[:-4]+ '.html'))
+   
 
 
 root.mainloop()
